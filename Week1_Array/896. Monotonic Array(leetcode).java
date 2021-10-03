16556e1a28e3703467a6ea12e67b61a2db8c54d8
Nandini Agarwal <nia78831@gmail.com>
2021-10-03 19:41:53 +0530

    Create 896. Monotonic Array(leetcode).java

---
+++ Week1_Array/896. Monotonic Array(leetcode).java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        
+        int inc=0;
+        int dec=0;
+        for(int i=0; i<nums.length-1; i++)
+        {
+           
+            if(nums[i]>nums[i+1])
+                dec++;
+           else if(nums[i]<nums[i+1])
+                inc++;
+            else
+            {
+                inc++;
+                dec++;
+            }
+        
+    }
+        if(inc==nums.length-1 || dec==nums.length-1)
+            return true;
+        else 
+            return false;
+        
+        
+    }
+    
+}
