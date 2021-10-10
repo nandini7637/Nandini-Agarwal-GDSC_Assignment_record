cd7f73a206cf4e8ded544a4412340b31a9429b19
Nandini Agarwal <nia78831@gmail.com>
2021-10-10 09:53:03 +0530

    Update 121. Best Time to Buy and Sell Stock.java

--- Week2 Searching and Sorting/121. Best Time to Buy and Sell Stock.java
+++ Week2 Searching and Sorting/121. Best Time to Buy and Sell Stock.java
@@ -2,24 +2,21 @@
 
 
 class Solution {
-    public int maxProfit(int[] arr) {
-        
-        int least =0;
-        int diff=0;
-        int j=0;
-        for(int i=1;i<arr.length;i++)
+public int maxProfit(int[] arr) {
+
+    int least =0;  // Stores index of smallest value
+    int diff=0;
+    int j=0;
+    for(int i=1;i<arr.length;i++)
+    {
+        if(arr[i]<arr[least])  // updates least
         {
-            if(arr[i]<arr[least])
-            {
-                least=i;
-                continue;
-            }
-            
-            if(diff<arr[i]-arr[least])
-                diff = arr[i]-arr[least];
-        
-            
+            least=i;
+            continue;
         }
-        return diff;
+        if(diff<arr[i]-arr[least]) // Compares difference
+            diff = arr[i]-arr[least];
     }
+    return diff;
+}
 }
