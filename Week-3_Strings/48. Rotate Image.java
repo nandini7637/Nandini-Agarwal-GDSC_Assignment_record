f681cc6af4d78addf9c13e5ca43dc28b83dc5813
Nandini Agarwal <nia78831@gmail.com>
2021-11-03 16:02:36 +0530

    Create 48. Rotate Image.java

---
+++ Week-3_Strings/48. Rotate Image.java
@@ -0,0 +1,37 @@
+Q link: https://leetcode.com/problems/rotate-image/
+
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=i;j<matrix[0].length;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        
+        for(int i=0;i<matrix.length;i++)
+        {
+           for(int j=0;j<matrix[0].length/2;j++)
+           {
+               int rev = matrix[i][j];
+               matrix[i][j] = matrix[i][matrix[0].length-1-j];
+               matrix[i][matrix[0].length-1-j]=rev;
+           }
+        }
+        
+        for(int i=0;i<matrix.length;i++)
+        {
+           for(int j=0;j<matrix[0].length;j++)
+           {
+               System.out.print(matrix[i][j]);
+           }
+            System.out.println();
+        }
+        
+    }
+}
